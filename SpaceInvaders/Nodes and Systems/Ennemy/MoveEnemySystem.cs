1736cad7b24fab2019154281d5ceeb2307c70ebe
rivettie <rivettie@pc5401w.lan.esiee.fr>
2018-11-23 11:56:27 +0100

    fix sortie ecran

--- SpaceInvaders/Nodes and Systems/Ennemy/MoveEnemySystem.cs
+++ SpaceInvaders/Nodes and Systems/Ennemy/MoveEnemySystem.cs
@@ -29,7 +29,7 @@ namespace SpaceInvaders.Nodes_and_Systems.Ennemy
                 }
 
                 Vecteur2D tempPos = n.TransformComponent.Position + movementVector * n.VelocityComponent.Velocity;
-                if (tempPos.x > RenderForm.instance.Width - n.RenderComponent.sprite.Width - 25)
+                if (tempPos.x > RenderForm.instance.Width - n.RenderComponent.sprite.Width - 25)//-25 pour empecher le bug de fenetre ou la sprite sort a moitié
                 {
                     foreach (MoveEnemyNode no in listNode)
                     {
